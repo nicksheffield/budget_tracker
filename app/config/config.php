fc22ac3e3f92d89469e449c30a9ae0adb9516dd5
Nick Sheffield <numbereft@gmail.com>
2014-07-13 01:48:44 +1200

    removed hidden lines from config.php

--- app/config/config.php
+++ app/config/config.php
@@ -16,8 +16,3 @@ $config['db']['hostname']      = 'localhost';
 $config['db']['username']      = '';
 $config['db']['password']      = '';
 $config['db']['database']      = '';
-
-require(APP_URL.'/config/db_creds.php'); #gitignore
-$config['db']['username']      = DB_USERNAME; #gitignore
-$config['db']['password']      = DB_PASSWORD; #gitignore
-$config['db']['database']      = DB_DATABASE; #gitignore
