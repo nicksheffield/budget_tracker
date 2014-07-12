50ed55c08a047fac3b71efaa1906d26b9fa57310
Nick Sheffield <numbereft@gmail.com>
2014-07-13 01:47:56 +1200

    Trying to hide some lines

--- app/config/config.php
+++ app/config/config.php
@@ -17,13 +17,7 @@ $config['db']['username']      = '';
 $config['db']['password']      = '';
 $config['db']['database']      = '';
 
-
-# Because git makes it hard to ignore specific lines of a repo, and I can't use a .gitignore,
-# I need to actually store my db credentials in a separate file that IS gitignored. 
-
-# Feel free to delete this, and just enter your credentials as per usual in the $config['db'] array above.
-
-require(APP_URL.'/config/db_creds.php');
-$config['db']['username']      = DB_USERNAME;
-$config['db']['password']      = DB_PASSWORD;
-$config['db']['database']      = DB_DATABASE;
+require(APP_URL.'/config/db_creds.php'); #gitignore
+$config['db']['username']      = DB_USERNAME; #gitignore
+$config['db']['password']      = DB_PASSWORD; #gitignore
+$config['db']['database']      = DB_DATABASE; #gitignore
